22674aec411c3dce22a8ffd9d972f130ad887523
javierherrerosan <javierherrerosan@alumnos.upm.es>
2022-01-07 10:13:50 +0100

    Introduciendo formatos webp en imagenes

--- assets/js/cargador-imagenes.js
+++ assets/js/cargador-imagenes.js
@@ -1,109 +1,109 @@
 const galeriaData = [
     {
         "descripcion": "Sala de estar",
-        "src": "assets/img/optimized/piso/20211227_112622.png",
+        "src": "assets/img/optimized/piso/20211227_112622.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Sala de estar",
-        "src": "assets/img/optimized/piso/20211227_113001.png",
+        "src": "assets/img/optimized/piso/20211227_113001.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Entrada",
-        "src": "assets/img/optimized/piso/20211227_113248.png",
+        "src": "assets/img/optimized/piso/20211227_113248.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Entrada",
-        "src": "assets/img/optimized/piso/20211227_113307.png",
+        "src": "assets/img/optimized/piso/20211227_113307.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Cocina",
-        "src": "assets/img/optimized/piso/20211227_114326.png",
+        "src": "assets/img/optimized/piso/20211227_114326.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Cocina",
-        "src": "assets/img/optimized/piso/20211227_114443.png",
+        "src": "assets/img/optimized/piso/20211227_114443.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Habitación Principal",
-        "src": "assets/img/optimized/piso/20211227_110949.png",
+        "src": "assets/img/optimized/piso/20211227_110949.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Habitación Principal",
-        "src": "assets/img/optimized/piso/20211227_110726.png",
+        "src": "assets/img/optimized/piso/20211227_110726.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Habitación Principal",
-        "src": "assets/img/optimized/piso/20211227_110817.png",
+        "src": "assets/img/optimized/piso/20211227_110817.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Segunda Habitación",
-        "src": "assets/img/optimized/piso/20211227_114748.png",
+        "src": "assets/img/optimized/piso/20211227_114748.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Segunda Habitación",
-        "src": "assets/img/optimized/piso/20211227_114812.png",
+        "src": "assets/img/optimized/piso/20211227_114812.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Segunda Habitación",
-        "src": "assets/img/optimized/piso/20211227_114820.png",
+        "src": "assets/img/optimized/piso/20211227_114820.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Tercera Habitación",
-        "src": "assets/img/optimized/piso/20211227_111836.png",
+        "src": "assets/img/optimized/piso/20211227_111836.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Tercera Habitación",
-        "src": "assets/img/optimized/piso/20211227_111821.png",
+        "src": "assets/img/optimized/piso/20211227_111821.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Tercera Habitación",
-        "src": "assets/img/optimized/piso/20211227_111901.png",
+        "src": "assets/img/optimized/piso/20211227_111901.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Baño",
-        "src": "assets/img/optimized/piso/20211227_115007.png",
+        "src": "assets/img/optimized/piso/20211227_115007.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Baño",
-        "src": "assets/img/optimized/piso/20211227_115116.png",
+        "src": "assets/img/optimized/piso/20211227_115116.webp",
         "width": 1500,
         "height": 1125
     },
     {
         "descripcion": "Baño",
-        "src": "assets/img/optimized/piso/20211227_115132.png",
+        "src": "assets/img/optimized/piso/20211227_115132.webp",
         "width": 1500,
         "height": 1125
     }
